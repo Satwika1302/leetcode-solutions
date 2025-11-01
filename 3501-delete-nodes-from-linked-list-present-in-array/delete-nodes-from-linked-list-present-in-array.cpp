d05f436fad4c9bc0e3f8ffc6e461f7f0b3fd76a4
Satwika1302 <satwikasai1302@gmail.com>
2025-10-31 23:28:49 -0400

    Time: 131 ms (26.33%) | Memory: 277.3 MB (12.05%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -10,6 +10,16 @@
  */
 class Solution {
 public:
+// 1.Initialize a set valuesToRemove and populate it with the values of the nums array.
+// 2.While the head of the linked list is not null and the head's value is present in valuesToRemove:
+// 3.Move head to head.next.
+// 4.If the head is null, return null since all nodes have been removed.
+// 5.Start iterating from the head of the modified list:
+// 6.For each node current, check if the value of the next node (current.next) is in the valuesToRemove set.
+// 7.If it is, skip the next node by updating current.next to current.next.next
+// 8.If it is not, move the current pointer to the next node in the list.
+// Return the updated head of the list.
+
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
         unordered_map<int,int>mp;
         for(int k:nums){
