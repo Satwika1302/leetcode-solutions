2e812a4204845cfcb2cda5ac04104ef8e23b24c5
Satwika Davala <satwikasai1302@gmail.com>
2026-08-28 10:52:47 -0400

    Time: 3 ms (85.83%) | Memory: 8.4 MB (91.22%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int expand(string &s,int left,int right){
+     int count = 0;
+        while(left>=0 && right<=s.size()-1 && s[left]==s[right]){
+
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
+    int countSubstrings(string s) {
+        int count = 0;
+        
+
+        for(int i = 0;i<s.size();i++){
+            count+=expand(s,i,i);
+            count+=expand(s,i,i+1);
+        } 
+
+        return count;
+    }
+};
