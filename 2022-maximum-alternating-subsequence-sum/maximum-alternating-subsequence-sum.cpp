0e94ea4e874003a04bd356b20299102d07340f26
Satwika Davala <satwikasai1302@gmail.com>
2026-08-31 10:35:53 -0400

    Time: 238 ms (57.41%) | Memory: 157.2 MB (43.60%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -10,7 +10,11 @@ public:
     }
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<long long>> dp(n, vector<long long>(2, -1));
-        return solve(0,1,nums,n,dp);
+        vector<vector<long long>> dp(n+1, vector<long long>(2, 0));
+        for(int i=n-1;i>=0;i--){
+            dp[i][1] = max(nums[i] + dp[i+1][0], dp[i+1][1]);
+            dp[i][0] = max(-nums[i]+ dp[i+1][1], dp[i+1][0]);
+        }
+        return dp[0][1];
     }
 };
